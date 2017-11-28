82542567c4c515bcc49a12c0a0daba31991a9d27
shah0056 <awais.a.khaliq@gmail.com>
2017-11-28 17:08:22 +0500

    Addition of XMPP client

---
+++ js/converse.js
@@ -0,0 +1,5 @@
+
+    converse.initialize({
+        bosh_service_url: 'https://bind.conversejs.org', // Please use this connection manager only for testing purposes
+        show_controlbox_by_default: true
+    });
