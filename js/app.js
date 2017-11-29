9728c68b05d003387110b35e1dff983c6fd6bca7
shah0056 <awais.a.khaliq@gmail.com>
2017-11-29 13:00:44 +0500

    Custom session initial code.
    Breaks functionality for now

--- js/app.js
+++ js/app.js
@@ -1,10 +1,38 @@
 // replace these values with those generated in your TokBox Account
-var apiKey = "YOUR_API_KEY";
-var sessionId = "YOUR_SESSION_ID";
-var token = "YOUR_TOKEN";
+var apikey = "YOUR_API_KEY";
+var apisecret = "YOUR_API_KEY";
+
+var opentok = new opentok.OpenTok(apikey, apisecret);
+
+// Session Creation:
+var sessionId;
+opentok.createSession({mediaMode:"relayed"}, function(error, session) {
+  if (error) {
+    console.log("Error creating session:", error)
+  } else {
+    sessionId = session.sessionId;
+    console.log("Session ID: " + sessionId);
+  }
+});
+
+// Token Generation
+var token = opentok.generateToken(sessionId);
+
+//Send token+session viaxmpp here....
+
+
+//Wait for conformation from xmpp, then initialize the session...
+
 
 // (optional) add server code here
+
+// This needs to be controlled in an if conditional, once conformation is recieved via xmpp, only then will this initialize!
 initializeSession();
+
+
+
+
+
 
 // Handling all of our errors here by alerting them
 function handleError(error) {
@@ -17,7 +45,13 @@ function initializeSession() {
   var session = OT.initSession(apiKey, sessionId);
 
   // Subscribe to a newly created stream
-
+	session.on('streamCreated', function(event) {
+	  session.subscribe(event.stream, 'subscriber', {
+	    insertMode: 'append',
+	    width: '100%',
+	    height: '100%'
+	  }, handleError);
+	});
   // Create a publisher
   var publisher = OT.initPublisher('publisher', {
     insertMode: 'append',
@@ -36,10 +70,3 @@ function initializeSession() {
   });
 }
 
-session.on('streamCreated', function(event) {
-  session.subscribe(event.stream, 'subscriber', {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%'
-  }, handleError);
-});
